ebbfb23660c51a10100edf6ccfdee320358bc03a
shuliangfu <admin@shuliangfu.com>
2025-12-25 12:56:43 +0800

    fix: 修复 Navbar.tsx 中 menu 导入路径，添加 .ts 扩展名
    - Deno 要求导入本地文件时必须包含文件扩展名
    - 将 '../config/menu' 改为 '../config/menu.ts'

--- example/components/Navbar.tsx
+++ example/components/Navbar.tsx
@@ -5,6 +5,7 @@
 
 import { useEffect, useState } from "preact/hooks";
 import { toggleTheme } from "@dreamer/dweb/client";
+import { menus } from "../config/menu.ts";
 
 interface NavbarProps {
   /** 当前路径（服务端渲染时使用） */
